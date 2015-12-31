143963dd956a91afb2a08f63f3f4ab612bd3e4c8
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:39:47 +1300

    Converted to PSR-2

--- code/Result.php
+++ code/Result.php
@@ -1,20 +1,22 @@
 <?php
 
-class groupdocsDotNetViewer_Result extends ViewableData {
-	protected $width = false;
-	protected $height = false;    
-	protected $server_url = false;
+class groupdocsDotNetViewer_Result extends ViewableData
+{
+    protected $width = false;
+    protected $height = false;
+    protected $server_url = false;
 
-	
-	public function __construct($width, $height, $url) {
-		$this->width = $width;
-		$this->height = $height;
-		$this->server_url = $url;
-		parent::__construct();
-	}
-	
-	public function forTemplate() {
-		return "<iframe src='{$this->server_url}' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
-	}
-	
+    
+    public function __construct($width, $height, $url)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->server_url = $url;
+        parent::__construct();
+    }
+    
+    public function forTemplate()
+    {
+        return "<iframe src='{$this->server_url}' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
+    }
 }
--- code/groupdocsDotNetViewer.php
+++ code/groupdocsDotNetViewer.php
@@ -1,25 +1,25 @@
 <?php
 
-class groupdocsDotNetViewer {
-	
-	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
-
+class groupdocsDotNetViewer
+{
+    
+    public static function handle_shortcode($arguments, $url, $parser, $shortcode)
+    {
         $width = 600 ;
-		$height = 800;
+        $height = 800;
 
 
-		if(isset($arguments['width']) && !empty($arguments['width'])) {
-			$width = strip_tags(trim($arguments['width']));
-		}
+        if (isset($arguments['width']) && !empty($arguments['width'])) {
+            $width = strip_tags(trim($arguments['width']));
+        }
 
-		if(isset($arguments['height']) && !empty($arguments['height'])) {
-			$height = strip_tags(trim($arguments['height']));
-		}
+        if (isset($arguments['height']) && !empty($arguments['height'])) {
+            $height = strip_tags(trim($arguments['height']));
+        }
 
         $url = strip_tags(trim($url));
 
         $grpDotNetViewer = new groupdocsDotNetViewer_Result($width, $height, $url);
         return $grpDotNetViewer->forTemplate();
-
-	}
+    }
 }
